94e6f9effbcd6441773c7fb8c0f79cfbbc2014e8
Richard <liuyongcheng@rongcloud.cn>
2019-03-20 16:58:36 +0800

    debug

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Test
-some test for innocent one.
+# 1. Setup MySQL on CentOS
+```
+卸载  先停掉mysql进程   没有安装过的可以直接跳过
+pkill -9 mysqld
+rpm -qa|grep -i mysql
+卸载
+yum -y remove mysql-community-client-5.6.38-2.el7.x86_6
+下载mysql的repo源 这个安装的mysql5.7.20  /**纠正一下，这源下载的是最新的版本  ****/
+cd /usr/local/src/
+wget http://repo.mysql.com/mysql57-community-release-el7-8.noarch.rpm
+rpm -ivh mysql57-community-release-el7-8.noarch.rpm
+yum -y install mysql-server 
+```
---
+++ src/main/java/richard/test/classloader/Test.java
@@ -0,0 +1,25 @@
+package richard.test.classloader;
+
+/**
+ * Created by richard on 19/03/2019.
+ */
+public class Test{
+
+    public static void main(String[] arg){
+
+        ClassLoader c  = Test.class.getClassLoader();  //获取Test类的类加载器
+
+        System.out.println(c);
+
+        ClassLoader c1 = c.getParent();  //获取c这个类加载器的父类加载器
+
+        System.out.println(c1);
+
+        ClassLoader c2 = c1.getParent();//获取c1这个类加载器的父类加载器
+
+        System.out.println(c2);
+
+
+    }
+
+}
