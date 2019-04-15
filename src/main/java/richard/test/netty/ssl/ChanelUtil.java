42382fc6056dba8c3344bc3298e8741aeccdcf8e
Richard <liuyongcheng@rongcloud.cn>
2019-04-15 16:47:19 +0800

    debug

--- src/main/java/richard/test/netty/ssl/ChanelUtil.java
+++ src/main/java/richard/test/netty/ssl/ChanelUtil.java
@@ -24,7 +24,7 @@ public class ChanelUtil {
                     otherInfo = "channel is null.";
                 }
 
-                if(e instanceof ClosedChannelException) {
+                if (e instanceof ClosedChannelException) {
                     LOGGER.error("channel to " + otherInfo + " is closed",e);
                 } else {
                     LOGGER.error("timeout occurred during channel send msg, " + otherInfo,e);
@@ -37,13 +37,13 @@ public class ChanelUtil {
     }
 
     public static ChannelFuture writeMessage(Channel channel,ByteBuf msg) {
-        if(channel!=null){
+        if (channel!=null) {
             try {
                 return channel.writeAndFlush(msg).sync();
             } catch (Exception e) {
                 LOGGER.error("timeout occured during channel send msg. remote address is:" + ((InetSocketAddress)channel.remoteAddress()).toString(),e);
             }
-        }else{
+        } else {
             LOGGER.error("send msg failed, channel is disconnected or not connect, channel is null, please see caller log.");
         }
         return null;
--- src/main/java/richard/test/netty/ssl/ChannelHandler.java
+++ src/main/java/richard/test/netty/ssl/ChannelHandler.java
@@ -10,14 +10,12 @@ public class ChannelHandler extends SimpleChannelInboundHandler<Object> {
     static final Logger LOGGER = LoggerFactory.getLogger(ChannelHandler.class);
 
     @Override
-    protected void channelRead0(final ChannelHandlerContext ctx, final Object msg)
-        throws Exception {
-        System.out.println(msg.toString());
+    protected void channelRead0(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+        LOGGER.info("msg is {}", msg);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx,
-                                Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         LOGGER.error("channel " + ((InetSocketAddress)ctx.channel().remoteAddress()).toString() + " exception:",cause);
         ctx.close();
     }
