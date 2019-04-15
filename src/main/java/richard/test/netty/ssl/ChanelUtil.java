9ebca0428d8705dda601a86d751dce44011c597b
Richard <liuyongcheng@rongcloud.cn>
2019-04-15 16:53:01 +0800

    debug

--- src/main/java/richard/test/netty/ssl/ChanelUtil.java
+++ src/main/java/richard/test/netty/ssl/ChanelUtil.java
@@ -12,39 +12,39 @@ public class ChanelUtil {
     static final Logger LOGGER = LoggerFactory.getLogger(ChanelUtil.class);
 
     public static ChannelFuture writeMessage(Channel channel,String msg) {
-        if (channel!= null) {
-            try {
-                return channel.writeAndFlush(msg).sync();
-            } catch (Exception e) {
-                String otherInfo = "";
-
-                if(channel.remoteAddress() != null) {
-                    otherInfo = "remote address [" + ((InetSocketAddress)channel.remoteAddress()).toString() + "]";
-                } else {
-                    otherInfo = "channel is null.";
-                }
-
-                if (e instanceof ClosedChannelException) {
-                    LOGGER.error("channel to " + otherInfo + " is closed",e);
-                } else {
-                    LOGGER.error("timeout occurred during channel send msg, " + otherInfo,e);
-                }
-            }
-        } else {
+        if (channel== null) {
             LOGGER.error("send msg failed, channel is disconnected or not connect. channel is null, please see caller log.");
+            return null;
         }
+        try {
+            return channel.writeAndFlush(msg).sync();
+        } catch (Exception e) {
+            LOGGER.error("error", e);
+            String otherInfo;
+            if(channel.remoteAddress() == null) {
+                otherInfo = "channel is null.";
+            } else {
+                otherInfo = "remote address [" + ((InetSocketAddress)channel.remoteAddress()).toString() + "]";
+            }
+            if (e instanceof ClosedChannelException) {
+                LOGGER.error("channel to " + otherInfo + " is closed",e);
+            } else {
+                LOGGER.error("timeout occurred during channel send msg, " + otherInfo,e);
+            }
+        }
+
         return null;
     }
 
     public static ChannelFuture writeMessage(Channel channel,ByteBuf msg) {
-        if (channel!=null) {
-            try {
-                return channel.writeAndFlush(msg).sync();
-            } catch (Exception e) {
-                LOGGER.error("timeout occured during channel send msg. remote address is:" + ((InetSocketAddress)channel.remoteAddress()).toString(),e);
-            }
-        } else {
-            LOGGER.error("send msg failed, channel is disconnected or not connect, channel is null, please see caller log.");
+        if (channel == null) {
+            LOGGER.error("send msg failed, channel is disconnected or not connect. channel is null, please see caller log.");
+            return null;
+        }
+        try {
+            return channel.writeAndFlush(msg).sync();
+        } catch (Exception e) {
+            LOGGER.error("timeout occured during channel send msg. remote address is:" + ((InetSocketAddress)channel.remoteAddress()).toString(),e);
         }
         return null;
     }
