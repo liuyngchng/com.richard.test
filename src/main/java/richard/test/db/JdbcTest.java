cee430a565988f9d56f354cc4d1aaea91f58a090
Richard Liu <liuyngchng@hotmail.com>
2019-05-29 17:13:09 +0800

    debug

--- src/main/java/richard/test/db/JdbcTest.java
+++ src/main/java/richard/test/db/JdbcTest.java
@@ -29,4 +29,31 @@ public class JdbcTest {
 
         return connection;
     }
+
+    public void queryData(Connection connection, String sql) {
+        PreparedStatement pstm;
+        ResultSet rs;
+        try {
+            pstm = connection.prepareStatement(sql);
+            LOGGER.info("start to execute sql: {}", sql);
+            rs = pstm.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            LOGGER.info("column count is {}", metaData.getColumnCount());
+            StringBuilder header = new StringBuilder();
+            for (int i = 0; i < metaData.getColumnCount(); i ++) {
+                header.append(metaData.getColumnName(i + 1) + "(" + metaData.getColumnType(i + 1) + ") | ");
+            }
+            LOGGER.info("data list is as following.");
+            System.out.println(header.toString());
+            while (rs.next()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < metaData.getColumnCount(); i ++) {
+                    sb.append(rs.getString(i + 1) + " | ");
+                }
+                System.out.println(sb.toString());
+            }
+        } catch (Exception ex) {
+            LOGGER.error("error to execute sql {}", sql, ex);
+        }
+    }
 }
