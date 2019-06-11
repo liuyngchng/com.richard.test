8662e21059e0f97c31c387be35ed0d7dfab2f88d
Richard Liu <liuyngchng@hotmail.com>
2019-06-11 18:07:33 +0800

    debug

--- src/main/java/richard/test/db/OracleDbTest.java
+++ src/main/java/richard/test/db/OracleDbTest.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 public class OracleDbTest extends JdbcTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OracleDbTest.class);
-    private static String USERNAME = "hebsycx";
-    private static String PASSWORD = "hUbe6mYblW";
+    private static String USERNAME = "";
+    private static String PASSWORD = "";
     private static String DRVIER = "oracle.jdbc.driver.OracleDriver";
-    private static String URL = "jdbc:oracle:thin:@10.179.0.93:1521:hbcvsdb3";
+    private static String URL = "jdbc:oracle:thin:@10.179.0.3:1234:hbcvsdb3";
 
 
     public static void main(String[] args) {
