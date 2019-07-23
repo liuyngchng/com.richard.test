7f3b0de06c0f098151addb1a5c22e1dd76182889
Richard <liuyongcheng@rongcloud.cn>
2019-07-23 15:57:35 +0800

    add akka demo

---
+++ src/main/java/richard/test/akka/Client.java
@@ -0,0 +1,80 @@
+package richard.test.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.io.Tcp;
+import akka.io.TcpMessage;
+import akka.util.ByteString;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by richard on 23/07/2019.
+ * see https://doc.akka.io/docs/akka/snapshot/io-tcp.html?language=java
+ */
+public class Client extends AbstractActor {
+    /*这是网络地址的封装类*/
+    final InetSocketAddress remote;
+    /*这是创建actor所依赖的类*/
+    final ActorRef listener;
+
+    //通过静态方法的调用来实现客户端的创建
+    public static Props props(InetSocketAddress remote, ActorRef listener) {
+        return Props.create(Client.class, remote, listener);
+    }
+
+    //构造方法
+    public Client(InetSocketAddress remote, ActorRef listener) {
+        this.remote = remote;
+        this.listener = listener;
+
+        //客户端的actor的创建
+        final ActorRef tcp = Tcp.get(getContext().getSystem()).manager();
+        tcp.tell(TcpMessage.connect(remote), getSelf());
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+            .match(Tcp.CommandFailed.class, msg -> {
+                listener.tell("failed", getSelf());
+                getContext().stop(getSelf());
+
+            })
+            //接收到连接，进行回复及创建，调用下方的connected操作
+            .match(Tcp.Connected.class, msg -> {
+                listener.tell(msg, getSelf());
+                getSender().tell(TcpMessage.register(getSelf()), getSelf());
+                getContext().become(connected(getSender()));
+            })
+            .build();
+    }
+
+
+    //进行接受数据，根据类型进行对应操作
+    private Receive connected(final ActorRef connection) {
+        return receiveBuilder()
+            //发送给服务端数据的动作
+            .match(ByteString.class, msg -> {
+                //具体操作，msg为要发送给服务端的数据
+                connection.tell(TcpMessage.write((ByteString) msg), getSelf());
+            })
+            .match(Tcp.CommandFailed.class, msg -> {
+                // OS kernel socket buffer was full
+            })
+            //接收服务端数据后的动作
+            .match(Tcp.Received.class, msg -> {
+                //具体操作
+                listener.tell(msg.data(), getSelf());
+            })
+            .matchEquals("close", msg -> {
+                connection.tell(TcpMessage.close(), getSelf());
+            })
+            .match(Tcp.ConnectionClosed.class, msg -> {
+                getContext().stop(getSelf());
+            })
+            .build();
+    }
+
+}
---
+++ src/main/java/richard/test/akka/Server.java
@@ -0,0 +1,79 @@
+package richard.test.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.io.Tcp;
+import akka.io.TcpMessage;
+import akka.util.ByteString;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by richard on 23/07/2019.
+ * see https://doc.akka.io/docs/akka/snapshot/io-tcp.html?language=java
+ */
+public class Server extends AbstractActor {
+
+    final ActorRef manager;
+
+    public Server(ActorRef manager) {
+        this.manager = manager;
+    }
+
+    public static Props props(ActorRef manager) {
+        return Props.create(Server.class, manager);
+    }
+
+    @Override
+    public void preStart() throws Exception {
+        final ActorRef tcp = Tcp.get(getContext().getSystem()).manager();
+        //老样子，是用来通知进行创建服务端actor
+        tcp.tell(TcpMessage.bind(getSelf(),
+            new InetSocketAddress("localhost", 0), 100), getSelf());
+    }
+
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+            .match(Tcp.Bound.class, msg -> {
+                manager.tell(msg, getSelf());
+
+            })
+            .match(Tcp.CommandFailed.class, msg -> {
+                getContext().stop(getSelf());
+
+            })
+            .match(Tcp.Connected.class, conn -> {
+                //有客户端连接
+                manager.tell(conn, getSelf());
+                //这个handler为自己实现的actor，即可指定让某个actor来处理接收的数据;
+                final ActorRef handler = getContext().actorOf(
+                    Props.create(SimplisticHandler.class));
+                //注册一个handler来进行接收客户端传来的数据.
+                getSender().tell(TcpMessage.register(handler), getSelf());
+            })
+            .build();
+    }
+    static class SimplisticHandler extends AbstractActor {
+        @Override
+        public Receive createReceive() {
+            return receiveBuilder()
+                .match(
+                    Tcp.Received.class,
+                    msg -> {
+                        final ByteString data = msg.data();
+                        System.out.println(data);
+                        getSender().tell(TcpMessage.write(data), getSelf());
+                    })
+                .match(
+                    Tcp.ConnectionClosed.class,
+                    msg -> {
+                        getContext().stop(getSelf());
+                    })
+                .build();
+        }
+    }
+
+}
