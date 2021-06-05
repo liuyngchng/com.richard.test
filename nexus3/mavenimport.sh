ebdfcfe0b45e63b74a047c097a94dd7327d82aa9
richard <liuyngchng@hotmail.com>
2021-06-05 14:54:44 +0800

    add comment to mavenimport.sh

--- nexus3/mavenimport.sh
+++ nexus3/mavenimport.sh
@@ -2,6 +2,13 @@
 # copy and run this script to the root of the repository directory containing files
 # this script attempts to exclude uploading itself explicitly so the script name is       important
 # Get command line params
+
+# put mavenimport.sh in folder '/home/$USER/.m2/repository', and then
+# chmod +x mavenimport.sh
+# ./mavenimport.sh -u admin -p admin123 -r http://172.20.10.6:8081/repository/my_repo/
+```
+
+
 while getopts ":r:u:p:" opt; do
   case $opt in
     r) REPO_URL="$OPTARG"
