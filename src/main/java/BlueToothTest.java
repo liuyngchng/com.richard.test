90ccc535f98cf5342b5d3adf692f80ae1f775eb1
Richard <liuyngchng@hotmail.com>
2022-07-12 21:00:27 +0800

    debug

--- src/main/java/BlueToothTest.java
+++ src/main/java/BlueToothTest.java
@@ -6,6 +6,10 @@ import java.math.BigInteger;
  * 128_bit_value = 16_bit_value * 2^96 + Bluetooth_Base_UUID
  * 2^96 = 2^4^24,也就是16进制左移24位
  * http://t.zoukankan.com/jiangu66-p-3170403.html
+ * 16bit uuid for members
+ * https://btprodspecificationrefs.blob.core.windows.net/assigned-values/16-bit%20UUID%20Numbers%20Document.pdf
+ *
+ * https://www.bluetooth.org/DocMan/handlers/DownloadDoc.ashx?doc_id=521059
  *
  * #蓝牙串口服务
  * SerialPortServiceClass_UUID = ‘{00001101-0000-1000-8000-00805F9B34FB}’
