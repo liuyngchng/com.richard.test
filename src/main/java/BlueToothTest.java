6b7018e5f6055cbbd28132b49b1df7315d4578d1
Richard <liuyngchng@hotmail.com>
2022-07-12 20:59:28 +0800

    debug

---
+++ src/main/java/BlueToothTest.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+
+/**
+ * 128_bit_value = 16_bit_value * 2^96 + Bluetooth_Base_UUID
+ * 2^96 = 2^4^24,也就是16进制左移24位
+ * http://t.zoukankan.com/jiangu66-p-3170403.html
+ *
+ * #蓝牙串口服务
+ * SerialPortServiceClass_UUID = ‘{00001101-0000-1000-8000-00805F9B34FB}’
+ */
+public class BlueToothTest {
+
+    public static void main(String[] args) {
+
+        String blueToothBaseUUID = "00000000-0000-1000-8000-00805F9B34FB";
+        BigInteger a = new BigInteger("1000800000805F9B34FB", 16);
+
+        String sdp = "0x0001";
+        int sdpNum = 1; //Integer.parseInt(sdp);
+
+        double b = Math.pow(2, 96);
+        BigDecimal bigDecimal = new BigDecimal(b);
+        BigDecimal result = bigDecimal.add(new BigDecimal(a));
+        System.out.println(result);
+
+
+
+
+    }
+}
