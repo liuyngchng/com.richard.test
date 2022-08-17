c82ab38b48a4801e2b9877d679e2440453303401
rd <liuyngchng@hotmail.com>
2022-08-17 14:52:33 +0800

    debug

--- sikulix/ocr.py
+++ sikulix/ocr.py
@@ -1,8 +1,8 @@
 #Settings.OcrDataPath = "/home/rd/workspace/sikulix"
-#Settings.OcrLanguage = "chi_sim"
-Settings.OcrLanguage = "eng"
-
-r=Region(194,383,1030,39)
+Settings.OcrLanguage = "chi_sim"
+#Settings.OcrLanguage = "eng"
+r=Region(30,222,178,48)
+#r=Region(194,383,1030,39)
 lines = r.collectLines() # a list of match objects
 firstLineMatch = lines[0] # the region on screen containing the line
 firstLine = firstLineMatch.getText()
