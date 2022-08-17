82aedef18e9fb5ebf3f6a474f7716191a75af68d
rd <liuyngchng@hotmail.com>
2022-08-17 14:48:50 +0800

    debug

--- python/pandas_excel.py
+++ python/pandas_excel.py
@@ -1,40 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # pip3 install pandas
+import sys 
 import pandas as pd
-# 使用dtype，指定某一列的数据类型
+
+# display config
 pd.options.display.max_rows = 999
 pd.options.display.max_columns =100 
-dft=pd.read_excel('/home/rd/work/rpa/a10.xlsx', index_col=None, skiprows=3, skipfooter=28)
-print(dft.columns)
-dft1=dft[['序号', '客户名称', '用户号', '账户类型', '燃气费率', '电话', '厂商名称', '地址','业务类型']]
+
+#define constant
+original_file= '/home/rd/work/rpa/a10.xlsx'
+norm_file = 'norm_output.xlsx'
+main_col = ['序号', '客户名称', '用户号', '账户类型', '燃气费率', '电话', '厂商名称', '地址','业务类型']
+# read excel as a dataFrame
+dft=pd.read_excel(original_file, index_col=None, skiprows=3, skipfooter=28)
+all_col=set(dft.columns)
+other_col = list(all_col -set(main_col))    # get the remained column exinclude mail_col list from all_col
+other_col.append('序号')
+print(sys._getframe().f_lineno, end = '_')
+print(other_col)
+#print(dft.columns)
+
+# selet part of the columan as another dataFrame
+#dft1=dft[['序号', '客户名称', '用户号', '账户类型', '燃气费率', '电话', '厂商名称', '地址','业务类型']]
+dft1=dft[main_col]
+# fulfill the cell with value above when the merged cell be de-merged
 dft1.fillna(method='ffill',inplace=True)
-dft2=dft[['序号', 
-       '业务类型', '办理时间', '气量', '计费项目', '数量', '价格', '项目金额（账单金额+滞纳金）', '账单金额',
-              '滞纳金', '税率', '税额', '不含税金额', '合计费用', '付款方式', '付款方', '实缴金额', '余额', '创建人',
-                     '创建时间', '营业员', '商户订单号', '终端名称']]
+# create anotehr dataFrame
+
+dft2=dft[other_col]
 #dft1.to_excel('test.xlsx')
-#dft3=dft.set_index('序号').join(dft1.set_index('序号'))
+# merge two dataFrame, the same column be merged automatically
 dft3=dft1.merge(dft2, left_on='序号',right_on='序号', how='left')
-dft3.to_excel('a10_output.xlsx')
+# output the merged dataFrame
+dft3.to_excel(norm_file)
 #print(dft)
-df1=pd.read_excel('test.xls', index_col='姓名', sheet_name='成绩',dtype={'姓名': str, '成绩': float}) # 使用index_col=0，指定第1列作为索引列
-df2=pd.read_excel('test.xls', index_col=None, sheet_name='汇总') # 使用index_col=0，指定第1列作为索引列
-print("original")
-print(df1)
-print("type(df1)=", end='')
-print(type(df1))
-df1.drop('我多余', axis=1, inplace =True)
-df1['成绩'] = df1['成绩'].map(lambda x: 60 if x=='59.0' else x)
-print('after changed')
-print(df1)
-print('begin filter')
-df3=df1.filter(like='王', axis=0)
-print(df3)
-df2['总成绩']=df2.类型.apply(lambda x: df1.成绩.loc[df1.类型== x].sum())
-print("type(df2)=", end='')
-print(type(df2))
-with pd.ExcelWriter('output.xlsx') as writer:             # mode ='a' 追加 
-    df2.to_excel(writer, engine='xlsxwriter',sheet_name='Sheet_name_3')
-#df2['平均成绩']=df2.类型.apply(lambda x: df1.成绩.loc[df1.类型== x].average())
-print(df2)
+
+# start filter data in excel
+df1=pd.read_excel(norm_file, index_col='价格')
+print(sys._getframe().f_lineno, end = '_')
+print(df1.index.unique())
+#filters = ['2.58', '']
+df2=df1.filter(like='2.58', axis=0)
+sum1 = df2['合计费用'].sum()
+print(sys._getframe().f_lineno, end = '_')
+print('2.58_sum=', end=' ')
+print(sum1)
+#df2.to_excel('test.xlsx')
