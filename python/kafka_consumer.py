e629bd4c2e17d7e98788d80dc75164d1acd87132
rd <whoami@hotmail.com>
2023-08-30 09:37:55 +0800

    debug

---
+++ python/kafka_consumer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#       #!/usr/bin/env python
+#       author whoami
+#       pip3 install pip3 install confluent-kafka
+#       ./kafks_consumer.py kafka.cfg
+import sys
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+    print("args", args)
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    config.update(config_parser['consumer'])
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+    print("consumer_config", config)
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(consumer, partitions):
+        if args.reset:
+            for p in partitions:
+                p.offset = OFFSET_BEGINNING
+            consumer.assign(partitions)
+
+    # Subscribe to topic
+    # topic = "test-up-rpt-dt"
+    topic = "reg-info"
+    consumer.subscribe([topic], on_assign=reset_offset)
+    print("consumer.subscribe", topic)
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting...")
+            elif msg.error():
+                print("ERR,", msg.error())
+                break
+            else:
+                # Extract the (optional) key and value, and print.
+
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:12} offset = {offset}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8'), offset=msg.offset()))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
