0151ed7afba2584f611893125a0905e6e9a4a17c
rd <whoami@hotmail.com>
2023-11-23 16:35:59 +0800

    debug

--- cpp/http/http.c
+++ cpp/http/http.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include "tcp.h"
 
 void readline(char *s, char *t, int n, int l) {
     int i=0;    // s index
@@ -62,6 +62,40 @@ void geturi(char *s, char *t, int n) {
 }
 
 
+// HTTP/1.1 200 
+// Set-Cookie: JSESSIONID=FCC697ED16FE18D3FA21F951BF39E0AE; Path=/industry; HttpOnly
+// Content-Type: application/json;charset=UTF-8
+// Transfer-Encoding: chunked
+// Date: Thu, 23 Nov 2023 06:00:57 GMT
+
+// 206
+// {"code":200,"message":"test"}
+void readbody(char *s, char *t, int n) {
+    int i = 0;
+    int j = 0;
+    int k = 0;      // start flag
+    for (; i < strlen(s); i++){
+        if(i<3){
+            continue;
+        }
+        if(j>=n){
+            break;
+        }
+        if (k){
+            t[j++] = s[i];
+            continue;
+        }
+    
+        if (s[i-3] == '\r' && s[i-2] == '\n' && s[i-1] == '\r' && s[i] == '\n') {
+            k=1;
+        }
+        continue;
+    }
+    t[j]='\0';
+}
+
+
+
 /* 
 GET / HTTP/1.1
 Host: localhost:8083
@@ -81,11 +115,18 @@ Host: passport.baidu.com
 username=admin&password=admin 
 */
 
-/**
- * http request 
- **/
-void httpreq(char *method, char *host, char *path, char *param, char *result){
 
-
-
+char *req(char *ip, int port, char *method, char *path, char *body, char *resp, int n) {
+    char req[1024] = {0};
+    sprintf(req, 
+        "%s %s HTTP/1.1\r\nHost: %s:%d\r\nContent-Type: application/json\r\nContent-Length:%ld\r\n\r\n%s",
+        method,path,ip, port, strlen(body),body
+    );
+    printf("[%s-%d] req_msg\n%s\n",filename(__FILE__), __LINE__, req);
+    char raw_resp[4096] = {0};
+    writemsg(ip, port, req, raw_resp);
+    char raw_body[4096] = {0};
+    readbody(raw_resp, raw_body, sizeof(raw_body));
+    readline(raw_body, resp, n, 2);
+    return resp;
 }
--- cpp/http/main.c
+++ cpp/http/main.c
@@ -1,12 +1,13 @@
 #include "tcp.h"
 #include "http.h"
 #include<stdio.h>
-#include<string.h>
 
 // http request test
-void myhttpreq();
+char *myhttpreq(char *response);
 
-void main(int argc, char* argv[]) {
+void httpreq();
+
+int main(int argc, char* argv[]) {
     int i=0;
     if(i) {
         printf("OK");
@@ -19,19 +20,37 @@ void main(int argc, char* argv[]) {
     // geturi(s, t, 10);
     // printf("t, %s\n", t);
     // startsrv();
-    myhttpreq();
-
+    // char resp[8096]={0};
+    // myhttpreq(resp);
+    
+    httpreq();
+    
+    return 0;
 }
 
-void myhttpreq() {
+void httpreq(){
+    char resp[8096] = {0};
+    // myhttpreq(resp);
+    char *ip = "11.10.36.2";
+    int port = 19061;
+    char *method="POST";
+    char *path="/industry/api/upMsgProcess";
+    char *bd = "{\"data\":\"6800EB03008188C1E000E1E55B9988776655443300110101000000000013FFFFFFFF03149A0FEC0100010001010000000000000000584AD45FB958C43F878129C981AA7587AC372EF3C21116C07E8EC7A55BB0BEE5BB50467015B1A81172D66EDD9B169E0C07311B501EB6EEBA5FB0A6D86BD1E15CEF058E5EBE6E57391861501FFA388EB61EB817A691A4DC92274579EAEAE681C9DC49F83CF6C7E5CD6631291A28D15DE8144EF11806CFC5C8D965282448E1F558A68852B243A1F657B2B68FC2E7DD721B8A1EE1254C36D8C7C3CAB9E08B2F7DBF8CA378C9477FC86D08A98B480D5E62464E189361000016\",\"transId\":\"T20211212091414-89F4BE38C6964238\",\"deviceId\":\"89F4BE38C6964238\",\"timestamp\":1635756025000}";
+    // char reqeust[1024]={0};
+    // req(resp);
+    req(ip, port, method, path, bd, resp, sizeof(resp));
+    printf("[%s-%d]read bodytxt\n%s\n", filename(__FILE__), __LINE__, resp);
+}
+
+char *myhttpreq(char *response) {
     // char *ip = "127.0.0.1";
     // int port = 8083;
     // char *req = "GET / HTTP/1.1\r\nHost: localhost:8083\r\nAccept:application/json\r\n\r\n";
 
     char *ip = "11.10.36.2";
     int port = 19061;
-    char *header = "POST /industry/api/upMsgProcess HTTP/1.1\r\nHost: 11.10.36.2:19061\r\nContent-Type: application/json\r\n";
     char *body = "{\"data\":\"6800EB03008188C1E000E1E55B9988776655443300110101000000000013FFFFFFFF03149A0FEC0100010001010000000000000000584AD45FB958C43F878129C981AA7587AC372EF3C21116C07E8EC7A55BB0BEE5BB50467015B1A81172D66EDD9B169E0C07311B501EB6EEBA5FB0A6D86BD1E15CEF058E5EBE6E57391861501FFA388EB61EB817A691A4DC92274579EAEAE681C9DC49F83CF6C7E5CD6631291A28D15DE8144EF11806CFC5C8D965282448E1F558A68852B243A1F657B2B68FC2E7DD721B8A1EE1254C36D8C7C3CAB9E08B2F7DBF8CA378C9477FC86D08A98B480D5E62464E189361000016\",\"transId\":\"T20211212091414-89F4BE38C6964238\",\"deviceId\":\"89F4BE38C6964238\",\"timestamp\":1635756025000}";
+    char *header = "POST /industry/api/upMsgProcess HTTP/1.1\r\nHost: 11.10.36.2:19061\r\nContent-Type: application/json\r\n";
     char req[1024] = {0};
     char len[8] = {0};
     sprintf(len, "%ld", strlen(body));
@@ -40,9 +59,10 @@ void myhttpreq() {
     strcat(req, len);
     strcat(req, "\r\n\r\n");
     strcat(req, body);
-    char response[8096] = {0};
     printf("write msg\n%s\n", req);
     writemsg(ip, port, req, response);
     printf("\nread msg\n%s\n", response);
-    return;
+    return response;
 }
+
+
