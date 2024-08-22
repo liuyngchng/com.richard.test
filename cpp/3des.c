8ffe09a6af0a0455be904ccc3227d4d8068e4914
richard <whoami@hotmail.com>
2024-08-22 09:06:45 +0800

    debug

--- cpp/3des.c
+++ cpp/3des.c
@@ -133,7 +133,7 @@ static char s_box[8][4][16] = {
  */
 static char p_tbl[32] = {
 	16, 7,20,21,29,12,28,17, 1,15,23,26, 5,18,31,10,
-	2, 8,24,14,32,27, 3, 9,19,13,30, 6,22,11, 4,25
+	 2, 8,24,14,32,27, 3, 9,19,13,30, 6,22,11, 4,25
 };
 
 /**
@@ -146,10 +146,6 @@ void set_key(char my_key[8]);
  */
 void byte_to_bit(bool* data_out, char* data_in, int num);
 
-/**
- * bit 转换
- */
-void change_bit(bool* data_out, int num);
 
 /**
  * 位转换成字节
@@ -202,22 +198,20 @@ void bit_to_hex(char* data_out, bool* data_in, int num);
 void decrypt(char my_msg[8], char hex_msg[16]);
 
 
-int main()
-{
+int main() {
     int i = 0, j;
     char my_key[8] = {0};  //记录加密密钥；
     char you_key[8] = {0}; //解密密钥
     char my_msg[8] = {0}; //明文
     char msg_hex[16] = {0};//16进制的密文
-    printf("请输入你要加密的内容(8 bytes):\n");
+    printf("请输入加密内容(8 bytes):\n");
     scanf("%s", my_msg);
 //    gets(My_message);
-    printf("请输入你的加密密钥(8 bytes):\n");
+    printf("请输入加密密钥(8 bytes):\n");
     scanf("%s", my_key);
 //    gets(My_key);
     i = strlen(my_key);
-    while(i != 8)
-    {
+    while(i != 8) {
         printf("请输入加密密钥(8 Byte)\n");
 //        gets(My_key);
         scanf("%s", my_key);
@@ -227,8 +221,7 @@ int main()
     set_key(my_key);  //生成16轮的加密子密钥；
     encrypt(my_msg, msg_hex); //des的轮加密过程
     printf("经过加密的密文为:\n");
-    for(i = 0; i < 16; i++)
-    {
+    for(i = 0; i < 16; i++) {
         printf("%c ", msg_hex[i]);
     }
     printf("\n");
@@ -236,8 +229,7 @@ int main()
 //    gets(You_key);
     scanf("%s", you_key);
     i = strlen(you_key);
-    while(i != 8)
-    {
+    while(i != 8) {
         printf("请输入解密密钥(8 Byte)\n");
 //        gets(You_key);
         scanf("%s", you_key);
@@ -247,201 +239,138 @@ int main()
     set_key(you_key);  //生成16轮的解密子密钥；
     decrypt(my_msg, msg_hex);//解密;
     printf("解密结果为:\n");
-    for(i = 0; i < 8; i++)
-    {
+    for(i = 0; i < 8; i++) {
         printf("%c ", my_msg[i]);
     }
     printf("\n");
     return 0;
 }
 
-/*--------------------具体函数定义----------------------*/
-void bit_cpy(bool* data_out, bool* data_in, int num) //二进制数组拷贝
-{
+
+void bit_cpy(bool* data_out, bool* data_in, int num) {
     int i = 0;
-    for(i = 0; i < num; i++)
-    {
+    for (i = 0; i < num; i++) {
         data_out[i] = data_in[i];
     }
 
 }
-void change_bit(bool* data_out, int num) //二进制的位置进行转换；
-{
+
+void byte_to_bit(bool* data_out, char* data_in, int num) {
     int i, j;
-    static bool tmp[8] = {0};
-    for(i = 0; i < num / 8; i++)
-    {
-        bit_cpy(tmp, data_out, num / 8);
-        for(j = 0; j < num / 8; j++)
-        {
-            data_out[j] = tmp[num / 8 - 1 - j];
-        }
-        data_out += num / 8;
-    }
-}
-void byte_to_bit(bool* data_out, char* data_in, int num) //字节转位
-{
-    int i, j;
-    for(i = 0; i < num; i++)
-    {
+    for(i = 0; i < num; i++) {
         data_out[i] = (data_in[i / 8] >> (i % 8)) & 0x01;
     }
-    //Change_bit(Data_out,Num);
 }
-void bit_to_hex(char* data_out, bool* data_in, int num) //二进制转十六进制
-{
+void bit_to_hex(char* data_out, bool* data_in, int num) {
     int i;
-    for(i = 0; i < num / 4; i++)
-    {
+    for(i = 0; i < num / 4; i++) {
         data_out[i] = 0;
     }
-    for(i = 0; i < num / 4; i++)
-    {
+    for(i = 0; i < num / 4; i++) {
         data_out[i] = data_in[4 * i] + data_in[4 * i + 1] * 2 + data_in[4 * i + 2] * 4 + data_in[4 * i + 3] * 8;
-        if(data_out[i] % 16 > 9)
-        {
+        if (data_out[i] % 16 > 9) {
             data_out[i] = data_out[i] % 16 + '7';
-        }
-        else
+        } else {
             data_out[i] = data_out[i] % 16 + '0';
+        }
     }
 }
-void hex_to_bit(bool* data_out, char* data_in, int num) //十六进制转二进制
-{
+void hex_to_bit(bool* data_out, char* data_in, int num) {
     int i;
-    for(i = 0; i < num; i++)
-    {
-        if(data_in[i / 4] <= '9')
-        {
+    for (i = 0; i < num; i++) {
+        if (data_in[i / 4] <= '9') {
             data_out[i] = ((data_in[i / 4] - '0') >> (i % 4)) & 0x01;
-        }
-        else
-        {
+        } else {
             data_out[i] = ((data_in[i / 4] - '7') >> (i % 4)) & 0x01;
         }
     }
 }
-void bit_to_byte(char my_msg[8], bool* msg_in, int num) //位转换成字节
-{
+void bit_to_byte(char my_msg[8], bool* msg_in, int num) {
     int i = 0;
-    for(i = 0; i < (num / 8); i++)
-    {
+    for (i = 0; i < (num / 8); i++) {
         my_msg[i] = 0;
     }
-    for(i = 0; i < num; i++)
-    {
+    for (i = 0; i < num; i++) {
         my_msg[i / 8] |= msg_in[i] << (i % 8);
     }
 }
-void tbl_replace(bool* data_out, bool* data_in, const char* table, int num) // 置换算法
-{
+void tbl_replace(bool* data_out, bool* data_in, const char* table, int num) {
     int i = 0;
     static bool tmp[256] = {0};
-    for(i = 0; i < num; i++)
-    {
+    for(i = 0; i < num; i++){
         tmp[i] = data_in[table[i] - 1];
     }
     bit_cpy(data_out, tmp, num);
 }
-void loop_bit(bool* data_out, int mv_step, int len)
-{
+void loop_bit(bool* data_out, int mv_step, int len) {
     static bool tmp[256] = {0};
     bit_cpy(tmp, data_out, mv_step);
     bit_cpy(data_out, data_out + mv_step, len - mv_step);
     bit_cpy(data_out + len - mv_step, tmp, mv_step);
-    /*tmp=data_out;
-    tmp[mv_step]='\0';
-    data_out=data_out+mv_step;
-    data_out+(len-mv_step)=tmp;*/
 }
-void xor(bool* msg_out, bool* msg_in, int num)//执行异或
-{
+void xor(bool* msg_out, bool* msg_in, int num) {
     int i;
-    for(i = 0; i < num; i++)
-    {
+    for (i = 0; i < num; i++) {
         msg_out[i] = msg_out[i] ^ msg_in[i];
     }
 }
-void set_key(char my_key[8])
-{
+void set_key(char my_key[8]) {
     int i, j;
     static bool key_bit[64] = {0}; //Key的二进制缓存；
     static bool* key_bit_l, * key_bit_r;
     key_bit_l = &key_bit[0]; //key的左边28位；
     key_bit_r = &key_bit[28]; //key的右边28位；
     byte_to_bit(key_bit, my_key, 64);
-    /* Change_bit(Key_bit,64) ;//二进制的位置进行转换；
-     for(i=0;i<64;i++)
-     {
-      printf("%d ",Key_bit[i]);
-     }
-     printf("\n");
-     printf("\n");*/
     tbl_replace(key_bit, key_bit, pc1_tbl, 56);//pc-1 置换
-    for(i = 0; i < 16; i++)
-    {
+    for(i = 0; i < 16; i++) {
         loop_bit(key_bit_l, mv_tbl[i], 28);
         loop_bit(key_bit_r, mv_tbl[i], 28);
         tbl_replace(sub_key[i], key_bit, pc2_tbl, 48);//pc-2置换
     }
 }
-void s_change(bool* data_out, bool* data_in) //S盒变换
-{
+void s_change(bool* data_out, bool* data_in) {
     int i;
     int r = 0, c = 0;//S盒的行和列；
-    for(i = 0; i < 8; i++, data_in = data_in + 6, data_out = data_out + 4)
-    {
+    for (i = 0; i < 8; i++, data_in = data_in + 6, data_out = data_out + 4) {
         r = data_in[0] * 2 + data_in[5] * 1;
         c = data_in[1] * 8 + data_in[2] * 4 + data_in[3] * 2 + data_in[4] * 1;
         byte_to_bit(data_out, &s_box[i][r][c], 4);
     }
 }
-void f_change(bool data_out[32], bool data_in[48])   // f函数；
-{
+void f_change(bool data_out[32], bool data_in[48]) {
     int i;
-    static bool msg_e[48] = {0};  //存放E置换的结果；
-    tbl_replace(msg_e, data_out, e_tbl, 48);//E表置换
+    static bool msg_e[48] = {0};
+    tbl_replace(msg_e, data_out, e_tbl, 48);
     xor(msg_e, data_in, 48);
-    s_change(data_out, msg_e);                 // S盒变换
-    tbl_replace(data_out, data_out, p_tbl, 32);  //P置换
+    s_change(data_out, msg_e);
+    tbl_replace(data_out, data_out, p_tbl, 32);
 }
 
 
-void encrypt(char my_msg[8], char hex_msg[16])
-{
+void encrypt(char my_msg[8], char hex_msg[16]) {
     int i;
     static bool msg_bit[64] = {0};
     static bool* msg_bit_l = &msg_bit[0], * msg_bit_r = &msg_bit[32];
     static bool tmp[32] = {0};
     byte_to_bit(msg_bit, my_msg, 64);
-    /*Change_bit(Message_bit,64) ;//二进制的位置进行转换；
-    for(i=0;i<64;i++)
-    {
-     printf("%d ",Message_bit[i]);
-    }
-    printf("\n");
-    printf("\n");*/
     tbl_replace(msg_bit, msg_bit, ip_tbl, 64);
-    for(i = 0; i < 16; i++)
-    {
+    for (i = 0; i < 16; i++) {
         bit_cpy(tmp, msg_bit_r, 32);
         f_change(msg_bit_r, sub_key[i]);
         xor(msg_bit_r, msg_bit_l, 32);
         bit_cpy(msg_bit_l, tmp, 32);
     }
     tbl_replace(msg_bit, msg_bit, ipr_tbl, 64);
-    bit_to_hex(hex_msg, msg_bit, 64);//二进制转换成十六进制；
+    bit_to_hex(hex_msg, msg_bit, 64);
 }
-void decrypt(char my_msg[8], char hex_msg[16])// DES轮解密算法；
-{
+void decrypt(char my_msg[8], char hex_msg[16]) {
     int i = 0;
     static bool msg_bit[64] = {0};
     static bool* msg_bit_l = &msg_bit[0], * msg_bit_r = &msg_bit[32];
     static bool tmp[32] = {0};
     hex_to_bit(msg_bit, hex_msg, 64);
     tbl_replace(msg_bit, msg_bit, ip_tbl, 64);
-    for(i = 15; i >= 0; i--)
-    {
+    for(i = 15; i >= 0; i--) {
         bit_cpy(tmp, msg_bit_l, 32);
         f_change(msg_bit_l, sub_key[i]);
         xor(msg_bit_l, msg_bit_r, 32);
