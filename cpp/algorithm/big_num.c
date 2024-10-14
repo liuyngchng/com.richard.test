2c7b74f294f8a91d615546d97c8a396177751913
richard <whoami@hotmail.com>
2024-10-14 10:29:15 +0800

    debug

--- cpp/algorithm/big_num.c
+++ cpp/algorithm/big_num.c
@@ -18,7 +18,7 @@ struct bigNum {
  * 获取一个初始化的大数字对象，值为0
  * @return 大数对象，值为零
  */
-BigNum getZero() {
+BigNum get_zero() {
     BigNum res;
     res._arrLen = 2;  // 设定一开始的位数
     res.arr = (int *) malloc(sizeof(int) * res._arrLen);
@@ -31,7 +31,7 @@ BigNum getZero() {
  * 销毁一个大数对象
  * @param bigNum 大数对象地址
  */
-void destroyBigNum(BigNum *bigNum) {
+void destroy_big_num(BigNum *bigNum) {
     free(bigNum->arr);
 }
 
@@ -40,13 +40,13 @@ void destroyBigNum(BigNum *bigNum) {
  * 2, 4, 8, 16 每次空间不够时，都扩充成原来的两倍
  * @param bigNum 需要扩容的大数对象地址
  */
-void extendBigNum(BigNum *bigNum) {
+void extend_big_num(BigNum *bigNum) {
     bigNum->_arrLen *= 2;
     bigNum->arr = (int *) realloc(bigNum->arr, sizeof(int) * bigNum->_arrLen);
 }
 
 // 整形乘方计算
-int myPow(int n, int m) {
+int my_pow(int n, int m) {
     if (m == 0) {
         return 1;
     }
@@ -62,7 +62,7 @@ int myPow(int n, int m) {
  * @param m
  * @return 如果m是 169 则返回 3
  */
-int getIntWei(int m) {
+int count_int_bit(int m) {
     int n = 0;
     do {
         n++;
@@ -76,14 +76,14 @@ int getIntWei(int m) {
  * @param int 输入的int类型数
  * @return bigNum 大数对象
  */
-BigNum intToBigNum(int integer) {
-    int n = getIntWei(integer);
-    BigNum res = getZero();
+BigNum int_to_big_num(int integer) {
+    int n = count_int_bit(integer);
+    BigNum res = get_zero();
     while (res._arrLen < n) {
-        extendBigNum(&res);
+        extend_big_num(&res);
     }
     for (int i = 1; i <= n; i++) {
-        int dig = (integer % (myPow(10, i))) / myPow(10, (i - 1));
+        int dig = (integer % (my_pow(10, i))) / my_pow(10, (i - 1));
         res.arr[i - 1] = dig;
     }
     res.wei = n;
@@ -96,10 +96,10 @@ BigNum intToBigNum(int integer) {
  * 字符串只能是纯数字，如果出现其他字符会导致程序bug
  * @return 大数对象
  */
-BigNum stringToBigNum(char *string) {
-    BigNum res = getZero();
+BigNum str_to_big_num(char *string) {
+    BigNum res = get_zero();
     while (res._arrLen < strlen(string)) {
-        extendBigNum(&res);
+        extend_big_num(&res);
     }
     for (long long i = 0; i < strlen(string); i++) {
         res.arr[strlen(string) - i - 1] = (int) (string[i]) - 48;
@@ -112,7 +112,7 @@ BigNum stringToBigNum(char *string) {
  * 将大数打印成一行打印出来
  * @param bigNum 大数对象地址
  */
-void printBigNumLn(BigNum *bigNum) {
+void print_bit_num_ln(BigNum *bigNum) {
     for (long long i = bigNum->wei - 1; i >= 0; i--) {
         printf("%d", bigNum->arr[i]);
     }
@@ -124,7 +124,7 @@ void printBigNumLn(BigNum *bigNum) {
  * 包含了直观表示，内部数组存储结构，位数和当前数组长度。
  * @param bigNum 大数对象地址
  */
-void printBigNumObj(BigNum *bigNum) {
+void print_bit_num_obj(BigNum *bigNum) {
     for (long long i = bigNum->wei - 1; i >= 0; i--) {
         printf("%d", bigNum->arr[i]);
     }
@@ -142,10 +142,10 @@ void printBigNumObj(BigNum *bigNum) {
  * @param bigN 大数对象地址
  * 无返回值，函数调用直接修改大数对象自身的值
  */
-void bigNumAddSelf(BigNum *bigN) {
+void big_num_add_0_self(BigNum *bigN) {
     // 扩容检测
     if (bigN->arr[bigN->wei - 1] >= 5 && bigN->wei == bigN->_arrLen) {
-        extendBigNum(bigN);
+        extend_big_num(bigN);
     }
     int next = 0;  // 下一位进位现象 0 无进位，1有进位
     for (long long i = 0; i < bigN->wei; i++) {
@@ -174,7 +174,7 @@ void bigNumAddSelf(BigNum *bigN) {
  * @param b2 大数对象地址 提供复制
  * 无返回值，函数调用直接修改大数对象 b1 自身的值
  */
-void bigNumCopy(BigNum *b1, BigNum *b2) {
+void big_num_cpy(BigNum *b1, BigNum *b2) {
     if (b1->_arrLen < b2->_arrLen) {
         // 扩容1
         b1->_arrLen = b2->_arrLen;
@@ -197,7 +197,7 @@ void bigNumCopy(BigNum *b1, BigNum *b2) {
  * @param b2 大数对象地址
  * @return 如果 b1 > b2 返回 1 ; 如果 b1 < b2 返回 -1；相等返回 0
  */
-int bigNumCmp(BigNum *b1, BigNum *b2) {
+int big_num_cmp(BigNum *b1, BigNum *b2) {
     if (b1->wei > b2->wei) {
         return 1;
     } else if (b1->wei < b2->wei) {
@@ -221,18 +221,18 @@ int bigNumCmp(BigNum *b1, BigNum *b2) {
  * @param b2 大数对象地址
  * @return 返回一个新的大数对象
  */
-BigNum bigNumPlus(BigNum *b1, BigNum *b2) {
+BigNum big_num_add(BigNum *b1, BigNum *b2) {
     // 注意左边的参数的值比右边的要大
-    if (bigNumCmp(b1, b2) == -1) {
+    if (big_num_cmp(b1, b2) == -1) {
         BigNum *temp = b1;
         b1 = b2;
         b2 = temp;
     }
-    BigNum res = getZero();
+    BigNum res = get_zero();
     if (b1->wei == b1->_arrLen) {
-        extendBigNum(b1); // 最高位刚好达到数组了就直接扩容，不判断了
+        extend_big_num(b1); // 最高位刚好达到数组了就直接扩容，不判断了
     }
-    bigNumCopy(&res, b1);
+    big_num_cpy(&res, b1);
     // b2 要扩容到和res一样的水平
     if (b2->_arrLen < res._arrLen) {
         b2->_arrLen = res._arrLen;
@@ -243,7 +243,7 @@ BigNum bigNumPlus(BigNum *b1, BigNum *b2) {
         }
     }
     while (b2->_arrLen <= res._arrLen) {
-        extendBigNum(b2);
+        extend_big_num(b2);
     }
     int next = 0;  // 下一位进位现象 0 无进位，1有进位
     for (long long i = 0; i < res.wei; i++) {
@@ -271,15 +271,15 @@ BigNum bigNumPlus(BigNum *b1, BigNum *b2) {
  * @param b2 大数对象地址
  * 无返回值，直接通过地址修改了b1的值。
  */
-void bigNumAdd(BigNum *b1, BigNum *b2) {
+void big_num_add_0(BigNum *b1, BigNum *b2) {
     if (b1->wei == b1->_arrLen) {
-        extendBigNum(b1); // 最高位刚好达到数组了就直接扩容，不判断了
+        extend_big_num(b1); // 最高位刚好达到数组了就直接扩容，不判断了
     }
-    int flag = bigNumCmp(b1, b2);
+    int flag = big_num_cmp(b1, b2);
     if (flag == -1) {
         // 后边大，前面小
         while (b1->_arrLen < b2->_arrLen) {
-            extendBigNum(b1);
+            extend_big_num(b1);
         }
         // 前面0化
         for (long long i = b1->wei; i < b1->_arrLen; i++) {
@@ -313,7 +313,7 @@ void bigNumAdd(BigNum *b1, BigNum *b2) {
  * @param a 自觉保证是个位数
  * 无返回值，函数调用直接修改 bgn 的值
  */
-void bigNumMulDig(BigNum *bgn, int a) {
+void big_num_multi_dig(BigNum *bgn, int a) {
     // 乘以0要特殊处理
     if (a == 0) {
         bgn->wei = 1;
@@ -322,7 +322,7 @@ void bigNumMulDig(BigNum *bgn, int a) {
     }
     // 如果这个数最高位刚好顶满了数组，那么可能就会有数组溢出的风险，直接扩容
     if (bgn->wei == bgn->_arrLen) {
-        extendBigNum(bgn);
+        extend_big_num(bgn);
     }
     int next = 0;
     for (long long i = 0; i < bgn->wei; i++) {
@@ -349,12 +349,12 @@ void bigNumMulDig(BigNum *bgn, int a) {
  *  (大数=1124, a=3)  => 大数变成：1124000
  * 无返回值，函数调用直接修改 bgn 的值
  */
-void bigNumAppend0(BigNum *bgn, long long a) {
+void big_num_append0(BigNum *bgn, long long a) {
     if (a <= 0) {
         return;
     }
     while (bgn->wei + a > bgn->_arrLen) {
-        extendBigNum(bgn);
+        extend_big_num(bgn);
     }
     for (long long i = bgn->wei - 1; i >= 0; i--) {
         bgn->arr[i + a] = bgn->arr[i];
@@ -371,10 +371,10 @@ void bigNumAppend0(BigNum *bgn, long long a) {
  * @param b2
  * @return 返回大数乘法后的结果
  */
-BigNum bigNumMul(BigNum *b1, BigNum *b2) {
-    BigNum res = getZero();
+BigNum big_num_multi(BigNum *b1, BigNum *b2) {
+    BigNum res = get_zero();
     // 注意左边的参数的值比右边的要大
-    if (bigNumCmp(b1, b2) == -1) {
+    if (big_num_cmp(b1, b2) == -1) {
         BigNum *temp = b1;
         b1 = b2;
         b2 = temp;
@@ -382,11 +382,11 @@ BigNum bigNumMul(BigNum *b1, BigNum *b2) {
     for (long long i = 0; i < b2->wei; i++) {
         // 遍历b2的每一位
         int w = b2->arr[i];
-        BigNum newBg = getZero();
-        bigNumCopy(&newBg, b1);  // 被乘数b1复制一份
-        bigNumMulDig(&newBg, w);
-        bigNumAppend0(&newBg, i);
-        bigNumAdd(&res, &newBg);
+        BigNum newBg = get_zero();
+        big_num_cpy(&newBg, b1);  // 被乘数b1复制一份
+        big_num_multi_dig(&newBg, w);
+        big_num_append0(&newBg, i);
+        big_num_add_0(&res, &newBg);
     }
     return res;
 }
@@ -396,54 +396,54 @@ BigNum bigNumMul(BigNum *b1, BigNum *b2) {
  * @param n 数字
  * @return 阶乘的结果
  */
-BigNum jie(int n) {
-    BigNum res = getZero();
+BigNum factorial(int n) {
+    BigNum res = get_zero();
     res.arr[0] = 1;
     for (int i = 1; i <= n; i++) {
-        BigNum bigI = intToBigNum(i);
-        res = bigNumMul(&res, &bigI);
-        destroyBigNum(&bigI);
+        BigNum bigI = int_to_big_num(i);
+        res = big_num_multi(&res, &bigI);
+        destroy_big_num(&bigI);
     }
     return res;
 }
 
 int main() {
     // 大数相加
-    BigNum b1 = intToBigNum(1);
-    printBigNumObj(&b1);
-    BigNum b2 = intToBigNum(95599);
-    printBigNumObj(&b2);
-    BigNum b3 = bigNumPlus(&b1, &b2);
-    printBigNumObj(&b3);
+    BigNum b1 = int_to_big_num(1);
+    print_bit_num_obj(&b1);
+    BigNum b2 = int_to_big_num(95599);
+    print_bit_num_obj(&b2);
+    BigNum b3 = big_num_add(&b1, &b2);
+    print_bit_num_obj(&b3);
 
     // 大数乘以个位数
-    BigNum bg1 = intToBigNum(11665544);
-    bigNumMulDig(&bg1, 6);
-    printBigNumObj(&bg1);
+    BigNum bg1 = int_to_big_num(11665544);
+    big_num_multi_dig(&bg1, 6);
+    print_bit_num_obj(&bg1);
 
     // 大数乘以大数
-    BigNum bg3 = intToBigNum(166655466);
-    BigNum bg4 = intToBigNum(443335544);
-    BigNum bg5 = bigNumMul(&bg3, &bg4);
-    printBigNumObj(&bg5);
+    BigNum bg3 = int_to_big_num(166655466);
+    BigNum bg4 = int_to_big_num(443335544);
+    BigNum bg5 = big_num_multi(&bg3, &bg4);
+    print_bit_num_obj(&bg5);
 
     // 字符串版 大数乘以大数
-    BigNum bgn1 = stringToBigNum("8717829120915684686898899879870");
-    BigNum bgn2 = stringToBigNum("1555888888564684684684684888888");
-    BigNum bgn3 = bigNumMul(&bgn1, &bgn2);
-    printBigNumObj(&bgn3);
+    BigNum bgn1 = str_to_big_num("8717829120915684686898899879870");
+    BigNum bgn2 = str_to_big_num("1555888888564684684684684888888");
+    BigNum bgn3 = big_num_multi(&bgn1, &bgn2);
+    print_bit_num_obj(&bgn3);
 
     // 遍历输出阶乘结果
     // for (int i = 1; i < 1000; i++) {
-    //     BigNum result = jie(i);
+    //     BigNum result = factorial(i);
     //     printf("!%d\n", i);
-    //     // printBigNumObj(&result);
-    //     printBigNumLn(&result);
-    //     destroyBigNum(&result);
+    //     // print_bit_num_obj(&result);
+    //     print_bit_num_ln(&result);
+    //     destroy_big_num(&result);
     // }
 
     // 计算超大阶乘
-    BigNum result = jie(6000);
-    printBigNumLn(&result);
+    BigNum result = factorial(6000);
+    print_bit_num_ln(&result);
 }
 
