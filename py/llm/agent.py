56a5aaf9d89be47da3aa2980c4c964c11c8429d4
richard <whoami@hotmail.com>
2025-02-17 11:25:26 +0800

    debug

--- py/llm/agent.py
+++ py/llm/agent.py
@@ -26,9 +26,20 @@ graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 # 编译图
+print("compile graph")
 graph = graph_builder.compile()
 
+# 6. 可视化 graph, 涉及到联网，生成png图片
+try:
+    print("save graph")
+#    graph.get_graph().draw_mermaid_png(output_file_path="./my_graph.png")
+except e:
+    # This requires some extra dependencies and is optional
+    print("exception", e)
+    pass
+
 # 执行图
+print("execute graph")
 user_input = '介绍你自己'
 for event in graph.stream({"messages": [("user", user_input)]}):
     for value in event.values():
